560e0dd7c2edb033a10f3be0fac30860b697dbb9
Mark Maluenda <markmaluenda@Marks-MacBook-Pro.local>
2023-08-23 14:28:25 -0400

    Fixing example_route unit tests

--- app/test/routes/test_example_route.py
+++ app/test/routes/test_example_route.py
@@ -6,11 +6,11 @@ from flask import Response
 class TestExampleRoute(TestAPIResource):
     
     def test_get_example_status_code_200(self):
-        response = self.app.get('/api/example')
+        response = self.app.get('/example')
         self.assertEqual(response.status_code, 200)
         
     def test_get_example_proper_output(self):
-        response = self.app.get('/api/example')
+        response = self.app.get('/example')
         
         response_data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response_data['message'], 'bar')
