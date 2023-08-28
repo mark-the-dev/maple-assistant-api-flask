2304413fb0480dfd94729e90785a38fa234d2a1c
Mark Maluenda <markmaluenda@Marks-MacBook-Pro.local>
2023-08-28 12:33:16 -0400

    Created User table and unit tests

---
+++ app/test/models/test_user.py
@@ -0,0 +1,36 @@
+import unittest
+from .. import TestGroup
+
+from app.main import db
+from app.main.models.user import User
+
+class TestUser(TestGroup):
+    
+    def test_create_user(self):
+        # Arrange
+        username = "testuser"
+        password = "testpassword"
+        
+        # Act
+        user = User(
+            username=username,
+            password=password
+        )
+        
+        # Assert
+        self.assertEqual(username, user.username)
+        self.assertEqual(password, user.password)
+        
+    def test_user_repr(self):
+        # Arrange
+        user = User(
+            id=1,
+            username="testuser",
+            password="testpassword"
+        )
+        
+        # Act
+        repr_str = repr(user)
+        
+        # Assert
+        self.assertEqual("User[id=1, username=testuser]", repr_str)
